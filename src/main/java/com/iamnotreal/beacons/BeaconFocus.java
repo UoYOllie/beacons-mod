aa66d075a74d78b253bf6e7d208a3a40fad7365d
UoYOllie <oj613@york.ac.uk>
2024-02-21 20:27:10 +0000

    Added Template

---
+++ src/main/java/com/iamnotreal/beacons/BeaconFocus.java
@@ -0,0 +1,10 @@
+package com.iamnotreal.beacons;
+
+import net.minecraft.block.Block;
+
+public class BeaconFocus extends Block {
+    public BeaconFocus(Settings settings) {
+        super(settings);
+    }
+
+}

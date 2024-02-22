074e65389d892f0bd58b9612abf4d6808e0cdf57
UoYOllie <oj613@york.ac.uk>
2024-02-22 12:23:42 +0000

    beacon focus item renders correctly now BeaconFocus collision & outline shape also accurate now

--- src/main/java/com/iamnotreal/beacons/BeaconFocus.java
+++ src/main/java/com/iamnotreal/beacons/BeaconFocus.java
@@ -1,10 +1,39 @@
 package com.iamnotreal.beacons;
 
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
 import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.ShapeContext;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.shape.VoxelShape;
+import net.minecraft.util.shape.VoxelShapes;
+import net.minecraft.world.BlockView;
 
 public class BeaconFocus extends Block {
+
+    public static final VoxelShape SHAPE = VoxelShapes.union(
+            Block.createCuboidShape(0, 0, 0, 16, 2, 16),
+            Block.createCuboidShape(0, 1, 7, 6, 3, 9),
+            Block.createCuboidShape(10, 1, 7, 16, 3, 9),
+            Block.createCuboidShape(2, 2, 2, 14, 4, 14)
+    );
+
     public BeaconFocus(Settings settings) {
         super(settings);
     }
+
+    @Override
+    public VoxelShape getOutlineShape(BlockState state, BlockView world, BlockPos pos, ShapeContext context){
+        return SHAPE;
+    }
+
+    @Override
+    public VoxelShape getCollisionShape(BlockState state, BlockView world, BlockPos pos, ShapeContext context){
+        return SHAPE;
+    }
+
+
+
 
 }
